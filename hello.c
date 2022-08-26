1d7178f2ce896a7f5b6da56d8e702e9d24ce2715
Grant Kerrigan <gkerrigan2@huskers.unl.edu>
2022-08-26 17:05:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I am Grant Kerrigan!\n");
+    printf("My Major is Computer Science\n");
+
+    return 0;
+
+}
